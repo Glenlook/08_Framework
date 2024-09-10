4ced72d1e2cbedd049889f7b72d6714e00d9f20e
Glenlook <shawn9701@naver.com>
2024-09-10 17:18:31 +0900

    9월 10일 TodoList Spring Boot에서 Ajax 방식으로 변환 실습

--- TodoList-Ajax/src/main/java/edu/kh/todolist/mapper/TodoListMapper.java
+++ TodoList-Ajax/src/main/java/edu/kh/todolist/mapper/TodoListMapper.java
@@ -51,4 +51,10 @@ public interface TodoListMapper {
 
 	String searchTitle(int todoNo);
 
+	/** 전체 할 일 개수 조회
+	 * @return
+	 */
+	int getTotalCount();
+
+
 }
--- TodoList-Ajax/src/main/java/edu/kh/todolist/service/TodoListServiceImpl.java
+++ TodoList-Ajax/src/main/java/edu/kh/todolist/service/TodoListServiceImpl.java
@@ -73,5 +73,22 @@ public class TodoListServiceImpl implements TodoListService{
 		return mapper.searchTitle(todoNo);
 	}
 	
+	// 전체 할 일 개수 조회
+	@Override
+	public int getTotalCount() {
+		return mapper.getTotalCount();
+	}
+	
+	@Override
+	public int getCompleteCount() {
+		return mapper.selectCompleteCount();
+	}
+	
+	// 할 일 전체 목록
+	@Override
+	public List<Todo> getTodoList() {
+		return mapper.selectTodoList();
+	}
+	
 	
 }
