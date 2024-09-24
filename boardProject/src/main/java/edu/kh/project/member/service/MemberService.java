f2ca911f3289b3df349bd360b711fb4a94a75684
Glenlook <shawn9701@naver.com>
2024-09-24 17:50:18 +0900

    9월 24일 board 프로젝트 유효성 검사 실습

--- boardProject/src/main/java/edu/kh/project/member/service/MemberService.java
+++ boardProject/src/main/java/edu/kh/project/member/service/MemberService.java
@@ -10,5 +10,23 @@ public interface MemberService {
 	 * @return loginMember 또는 null(email 또는 pw 불일치)
 	 */
 	Member login(String memberEmail, String memberPw);
+
+	/** 회원 가입
+	 * @param inputMember
+	 * @return result
+	 */
+	int signUp(Member inputMember);
+
+	/** 이메일 중복 검사
+	 * @param email
+	 * @return count
+	 */
+	int emailCheck(String email);
+
+	/** 닉네임 중복 검사
+	 * @param nickname
+	 * @return count
+	 */
+	int nicknameCheck(String nickname);
 	
 }
